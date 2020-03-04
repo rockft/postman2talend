dc1ef5e8340cd0df0b27b1bc769319ad7b64c0c7
rockft <fantuan@gmail.com>
2020-03-04 12:15:02 +0800

    v 0.1

---
+++ postman2talend.php
@@ -0,0 +1,135 @@
+<?php
+$postmanFile = ($argv[1])?$argv[1]:'postman.json';
+$postmanString = file_get_contents(__DIR__.'/'.$postmanFile);
+$postmanJson = json_decode($postmanString);
+
+//var_dump($postmanJson);
+
+$talendJson = json_decode(
+'{
+    "version": 6,
+    "entities": [
+        {
+            "entity": {
+                "type": "Project",
+                "id": "d16782bf-c458-4f91-96fb-d1f96f5a758a",
+                "name": "postman project"
+            },
+            "children": []
+        }
+    ]
+}
+');
+
+//$talendJson->entities[0]->entity->id = uuid('postman project'.$postmanJson->name);
+$talendJson->entities[0]->entity->id = $postmanJson->id;
+$talendJson->entities[0]->entity->name = $postmanJson->name;
+
+$entityTemplate = '{
+    "entity": {
+        "type": "Request",
+        "method": {
+            "requestBody": true,
+            "link": "http://tools.ietf.org/html/rfc7231#section-4.3.3",
+            "name": "POST"
+        },
+        "body": {
+            "formBody": {
+                "overrideContentType": true,
+                "encoding": "application/x-www-form-urlencoded",
+                "items": []
+            },
+            "bodyType": "Text"
+        },
+        "uri": {
+            "query": {
+                "delimiter": "&",
+                "items": []
+            },
+            "scheme": {
+                "name": "http",
+                "version": "V11"
+            },
+            "host": "127.0.0.1",
+            "path": "/path"
+        },
+        "id": "eb704a00-496f-496a-9285-941ce78e13d6",
+        "name": "template",
+        "headers": []
+    }
+}';
+
+$itemTemplate = '{
+    "enabled": true,
+    "encoded": true,
+    "name": "",
+    "value": ""
+}';
+
+$headerTemplate = '{
+    "enabled": true,
+    "name": "",
+    "value": ""
+}';
+
+foreach ($postmanJson->requests as $pmItem) {
+    $entity = json_decode($entityTemplate);
+    $entity->entity->id = $pmItem->id;
+    $entity->entity->name = $pmItem->name;
+    $entity->entity->method->name = $pmItem->method;
+
+    // url parse
+    $url = parse_url($pmItem->url);
+    $entity->entity->uri->scheme->name = $url['scheme'];
+    $entity->entity->uri->host = $url['host'];
+    $entity->entity->uri->path = $url['path'];
+
+    // url params
+    if (!empty($url['query'])) {
+        $urlParams = explode('&', $url['query']);
+        foreach ($urlParams as $urlParam) {
+            $item = json_decode($itemTemplate);
+            $urlParam = explode('=', $urlParam);
+            $item->name = $urlParam[0];
+            $item->value = $urlParam[1];
+            $entity->entity->uri->query->items[] = $item;
+        }
+    }
+    foreach ($pmItem->data as $param) {
+        $item = json_decode($itemTemplate);
+        $item->name = $param->key;
+        $item->value = $param->value;
+        $entity->entity->uri->query->items[] = $item;
+    }
+
+    // headers
+    if (!empty($pmItem->headers))
+    {
+        $pmHeaders = explode("\n", trim($pmItem->headers));
+        foreach ($pmHeaders as $pmHeader) {
+            $header = json_decode($headerTemplate);
+            $pmHeader = explode(': ', $pmHeader);
+            $header->name = $pmHeader[0];
+            $header->value = $pmHeader[1];
+            $entity->entity->headers[] = $header;
+        }
+    }
+
+    //
+    $talendJson->entities[0]->children[] = $entity;
+}
+
+file_put_contents(__DIR__.'/postman2talend.json', json_encode($talendJson, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+echo 'success';
+
+/******************************************************************************** */
+
+function uuid($str)
+{
+    $str = md5($str);
+    for ($i=8; $i < 24; $i=$i+5) { 
+        $str = substr_replace($str,'-',$i,0);
+    }
+    return $str;
+    //return substr_replace(substr_replace(substr_replace(substr_replace(md5($str),'-',8,0),'-',13,0),'-',18,0),'-',23,0);
+}
